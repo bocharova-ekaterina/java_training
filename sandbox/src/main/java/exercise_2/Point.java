dcc53bc56784618cac00a8c13e5e9e73774d593a
bocharova-ekaterina <36412738+bocharova-ekaterina@users.noreply.github.com>
2021-10-03 23:59:44 +0700

    Задание №2 с использованием метода

--- sandbox/src/main/java/exercise_2/Point.java
+++ sandbox/src/main/java/exercise_2/Point.java
@@ -1,14 +1,16 @@
 package exercise_2;
 
 public class Point {
-
     public double x;
     public double y;
 
+    //кнструктор
     public Point(double x, double y) {
        this.x=x;
        this.y=y;
     }
-
-
+    // первый аргумент метода заменен на this
+    public double distance(Point point_2){
+        return Math.sqrt((point_2.x-this.x)*(point_2.x-this.x) + (point_2.y-this.y)*(point_2.y-this.y));
+    }
 }
