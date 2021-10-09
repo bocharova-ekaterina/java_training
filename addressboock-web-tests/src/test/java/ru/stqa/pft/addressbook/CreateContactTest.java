308b1d0416a0f56ebe6d4ccbd5a69b8f42b5a1ed
bocharova-ekaterina <36412738+bocharova-ekaterina@users.noreply.github.com>
2021-10-09 23:35:44 +0700

    Добавление класса TestBase (наследование)

--- addressboock-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateContactTest.java
+++ addressboock-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateContactTest.java
@@ -1,62 +1,8 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class CreateContactTest {
-  private WebDriver wd;
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
-
-  private void logout() {
-    wd.findElement(By.linkText("Logout")).click();
-  }
-
-  private void goToHomePage() {
-    wd.findElement(By.linkText("home page")).click();
-  }
-
-  private void fillContactForm(ContactData contactData) {
-    wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactData.getHome_phone());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile_phone());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-  }
-
-  @BeforeClass(alwaysRun = true)
-  public void setUp() throws Exception {
-    System.setProperty("webdriver.gecko.driver","C:\\Windows\\System32\\geckodriver.exe");
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-   login("admin", "secret");
-  }
+public class CreateContactTest extends TestBase {
 
   @Test
   public void testUntitledTestCase() throws Exception {
@@ -64,28 +10,5 @@ public class CreateContactTest {
     goToHomePage();
     logout();
   }
-
-
-  @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 }
+
--- addressboock-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateGroupTest.java
+++ addressboock-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateGroupTest.java
@@ -1,56 +1,9 @@
 package ru.stqa.pft.addressbook;
-
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class CreateGroupTest {
-  private WebDriver wd;
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    System.setProperty("webdriver.gecko.driver","C:\\Windows\\System32\\geckodriver.exe");
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("https://localhost/addressbook/");
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
-
-  private void goToGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
-  }
-
-  private void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
-  }
-
-  private void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  private void logout() {
-    wd.findElement(By.linkText("Logout")).click();
-  }
+ //наследует класс TestBase
+public class CreateGroupTest extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
@@ -60,33 +13,6 @@ public class CreateGroupTest {
     submitGroupCreation();
     goToGroupPage();
     logout();
-  }
-
-  private void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
   }
 
 }
