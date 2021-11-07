db01e879382a77836d7824b86d24c7860779b299
bocharova-ekaterina <36412738+bocharova-ekaterina@users.noreply.github.com>
2021-11-07 23:06:10 +0700

    Лекция 6.5 (Ошибка DataProvider)

--- addressboock-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTest.java
+++ addressboock-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTest.java
@@ -2,17 +2,36 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
 
 //наследует класс TestBase
 public class CreateGroupTest extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader =new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line!=null){
+     String[] split = line.split(";");
+     list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line=reader.readLine();
+    }
+    return list.iterator();
+
+  }
+
+  @Test(dataProvider="validGroups")
+  public void testGroupCreation(GroupData group) throws Exception {
     app.goTo().groupPage();
     Groups before= app.group().all();
-    GroupData group = new GroupData().withName("test2");
     app.group().create(group);
     app.goTo().groupPage();
     assertThat(app.group().count(), equalTo(before.size() +1));
@@ -20,11 +39,11 @@ public class CreateGroupTest extends TestBase {
     assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
   }
 
-  @Test
+  @Test(enabled = false)
   public void testBadGroupCreation() throws Exception {
     app.goTo().groupPage();
     Groups before= app.group().all();
-    GroupData group = new GroupData().withName("test2");
+    GroupData group = new GroupData().withName("test2'");
     app.group().create(group);
     app.goTo().groupPage();
     assertThat(app.group().count(), equalTo(before));
