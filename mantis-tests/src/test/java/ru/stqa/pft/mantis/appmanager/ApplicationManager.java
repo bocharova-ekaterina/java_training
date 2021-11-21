efd932be78e770a359ff20aa85ad0ba12252ae6b
bocharova-ekaterina <36412738+bocharova-ekaterina@users.noreply.github.com>
2021-11-21 23:24:58 +0700

    Добавление теста LoginTests

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.HttpSessionId;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -33,11 +35,19 @@ public class ApplicationManager {
         }else if(browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         }
-        wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseURL"));
     }
 
     public void stop() {
         wd.quit();
+    }
+
+    public Object getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public HttpSession newSession() {
+        return new HttpSession(this);
     }
 }
