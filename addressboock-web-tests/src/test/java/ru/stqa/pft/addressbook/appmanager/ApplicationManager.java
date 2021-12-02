2142d802457f6cd8650a50910a00aa4a1d7b0bf0
bocharova-ekaterina <36412738+bocharova-ekaterina@users.noreply.github.com>
2021-12-03 00:01:43 +0700

    fix dependencies 1

--- addressboock-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressboock-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -37,7 +37,7 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         dbHelper=new DbHelper();
-      System.setProperty("webdriver.gecko.driver","C:\\Windows\\System32\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver","C:\\Windows\\System32\\geckodriver.exe");
         if("".equals(properties.getProperty("selenium.server"))) {
             if (browser.equals(BrowserType.FIREFOX)) {
                 wd = new FirefoxDriver();
